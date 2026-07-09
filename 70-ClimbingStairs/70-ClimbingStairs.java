e7f9b5e3e207ceb441a4268b79fc6b694f42acc8
ARUN T <arundevil2007@gmail.com>
2026-07-09 10:07:49 +0530

    Create 70-ClimbingStairs

---
+++ 70-ClimbingStairs/70-ClimbingStairs.java
@@ -0,0 +1,18 @@
+// Last updated: 7/9/2026, 10:07:29 AM
+class Solution {
+    public int climbStairs(int n) {
+        int[] memo = new int[n + 1];
+        return helper(n, memo);
+    }
+    
+    private int helper(int n, int[] memo) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        if (memo[n] != 0) {
+            return memo[n];
+        }
+        memo[n] = helper(n - 1, memo) + helper(n - 2, memo);
+        return memo[n];
+    }
+}
