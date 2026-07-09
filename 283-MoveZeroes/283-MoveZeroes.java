d675a81656918066e74040a46c3e99e8855d1951
ARUN T <arundevil2007@gmail.com>
2026-07-09 10:05:39 +0530

    Create 283-MoveZeroes

---
+++ 283-MoveZeroes/283-MoveZeroes.java
@@ -0,0 +1,14 @@
+// Last updated: 7/9/2026, 10:05:18 AM
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n=nums.length,j=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0){
+                int t=nums[j];
+                nums[j]=nums[i];
+                nums[i]=t;
+                j++;
+            }
+        }
+    }
+}
