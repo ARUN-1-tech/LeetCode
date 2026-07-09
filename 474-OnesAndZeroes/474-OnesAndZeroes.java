cc2dbbbfc4a7b5bff300db9770cbc99c959e613c
ARUN T <arundevil2007@gmail.com>
2026-07-09 10:05:01 +0530

    Create 474-OnesAndZeroes

---
+++ 474-OnesAndZeroes/474-OnesAndZeroes.java
@@ -0,0 +1,16 @@
+// Last updated: 7/9/2026, 10:04:40 AM
+class Solution {
+    public int findMaxForm(String[] S, int M, int N) {
+        int[][] dp = new int[M+1][N+1];
+        for (String str : S) {
+            int zeros = 0, ones = 0;
+            for (char c : str.toCharArray())
+                if (c == '0') zeros++;
+                else ones++;
+            for (int i = M; i >= zeros; i--)
+                for (int j = N; j >= ones; j--)
+                    dp[i][j] = Math.max(dp[i][j], dp[i-zeros][j-ones] + 1);
+        }
+        return dp[M][N];
+    }
+}
