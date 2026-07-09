ade67d8d38188a8cc16dee47733646a01dd010ec
ARUN T <arundevil2007@gmail.com>
2026-07-09 10:04:26 +0530

    Create 747-MinCostClimbingStairs

---
+++ 747-MinCostClimbingStairs/747-MinCostClimbingStairs.java
@@ -0,0 +1,12 @@
+// Last updated: 7/9/2026, 10:04:06 AM
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int[] dp=new int[cost.length+1];
+        dp[0]=0;
+        dp[1]=0;
+        for(int i=2;i<=cost.length;i++){
+            dp[i]=Math.min(dp[i-1]+cost[i-1],dp[i-2]+cost[i-2]);
+        }
+        return dp[cost.length];
+    }
+}
