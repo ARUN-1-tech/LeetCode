48b1ae074a1652628ab0e1b2ce27a4d446afec84
ARUN T <arundevil2007@gmail.com>
2026-07-10 14:37:16 +0530

    File updated at 7/10/2026, 2:36:55 PM

--- 94-BinaryTreeInorderTraversal/94-BinaryTreeInorderTraversal.java
+++ 94-BinaryTreeInorderTraversal/94-BinaryTreeInorderTraversal.java
@@ -1,30 +1,29 @@
-// Last updated: 7/9/2026, 10:07:08 AM
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    List<Integer> ans;
-    public List<Integer> inorderTraversal(TreeNode root) {
-        ans=new ArrayList<>();
-        inorder(root);
-        return ans;
-    }
-    private void inorder(TreeNode root){
-        if(root==null) return;
-        inorder(root.left);
-        ans.add(root.val);
-        inorder(root.right);
-    }
-}
+// Last updated: 7/10/2026, 2:36:55 PM
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    public void dfs(TreeNode root,List<Integer> ans){
+18        if(root==null) return;
+19        dfs(root.left,ans);
+20        ans.add(root.val);
+21        dfs(root.right,ans);
+22    }
+23    public List<Integer> inorderTraversal(TreeNode root) {
+24        List<Integer> ans=new ArrayList<>();
+25        dfs(root,ans);
+26        return ans;
+27    }
+28}
