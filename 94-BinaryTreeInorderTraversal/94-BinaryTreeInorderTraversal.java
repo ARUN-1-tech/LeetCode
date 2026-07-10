0386ef0a3a2646853feb4700105673ded9e67f3c
ARUN T <arundevil2007@gmail.com>
2026-07-10 15:03:37 +0530

    File updated at 7/10/2026, 3:03:17 PM

--- 94-BinaryTreeInorderTraversal/94-BinaryTreeInorderTraversal.java
+++ 94-BinaryTreeInorderTraversal/94-BinaryTreeInorderTraversal.java
@@ -1,29 +1,15 @@
-// Last updated: 7/10/2026, 2:36:55 PM
-1/**
-2 * Definition for a binary tree node.
-3 * public class TreeNode {
-4 *     int val;
-5 *     TreeNode left;
-6 *     TreeNode right;
-7 *     TreeNode() {}
-8 *     TreeNode(int val) { this.val = val; }
-9 *     TreeNode(int val, TreeNode left, TreeNode right) {
-10 *         this.val = val;
-11 *         this.left = left;
-12 *         this.right = right;
-13 *     }
-14 * }
-15 */
-16class Solution {
-17    public void dfs(TreeNode root,List<Integer> ans){
-18        if(root==null) return;
-19        dfs(root.left,ans);
-20        ans.add(root.val);
-21        dfs(root.right,ans);
-22    }
-23    public List<Integer> inorderTraversal(TreeNode root) {
-24        List<Integer> ans=new ArrayList<>();
-25        dfs(root,ans);
-26        return ans;
-27    }
-28}
+// Last updated: 7/10/2026, 3:03:17 PM
+1class Solution {
+2    public int[] searchRange(int[] nums, int target) {
+3        int[] ans=new int[2];
+4        Arrays.fill(ans,-1);
+5        for(int i=0;i<nums.length;i++){
+6            if(nums[i]==target){
+7                if(ans[0]!=-1) ans[1]=i;
+8                else ans[0]=i;
+9            }
+10        }
+11        if(ans[0]!=-1 && ans[1]==-1) ans[1]=ans[0];
+12        return ans;
+13    }
+14}
