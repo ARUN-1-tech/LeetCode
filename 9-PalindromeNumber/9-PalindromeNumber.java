cc27491ee6195ef82ea7ed67c399eff21e1c44cb
ARUN T <arundevil2007@gmail.com>
2026-07-10 16:18:19 +0530

    File updated at 7/10/2026, 4:17:59 PM

--- 9-PalindromeNumber/9-PalindromeNumber.java
+++ 9-PalindromeNumber/9-PalindromeNumber.java
@@ -1,12 +1,15 @@
-// Last updated: 7/10/2026, 3:49:51 PM
+// Last updated: 7/10/2026, 4:17:59 PM
 1class Solution {
-2    public boolean isPalindrome(int x) {
-3        int a=0;
-4        int n=x;
-5        while(n>0){
-6            a=(a*10)+(n%10);
-7            n=n/10;
-8        }
-9        return x==a;
-10    }
-11}
+2    public int[] searchRange(int[] nums, int target) {
+3        int[] ans=new int[2];
+4        Arrays.fill(ans,-1);
+5        for(int i=0;i<nums.length;i++){
+6            if(nums[i]==target){
+7                if(ans[0]!=-1) ans[1]=i;
+8                else ans[0]=i;
+9            }
+10        }
+11        if(ans[0]!=-1 && ans[1]==-1) ans[1]=ans[0];
+12        return ans;
+13    }
+14}
