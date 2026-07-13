fa364d8ec099376ed1c127a6c98767089cea5534
ARUN T <arundevil2007@gmail.com>
2026-07-13 21:40:41 +0530

    File updated at 7/13/2026, 9:40:22 PM

--- 50-Pow(x,n)/50-Pow(x,n).java
+++ 50-Pow(x,n)/50-Pow(x,n).java
@@ -1,14 +1,13 @@
-// Last updated: 7/13/2026, 9:30:39 PM
+// Last updated: 7/13/2026, 9:40:22 PM
 1class Solution {
-2    public double myPow(double x, int n) {
-3        return solve(x, (long) n);
-4    }
-5
-6    private double solve(double x, long n) {
-7        if (n == 0) return 1.0;
-8        if (n < 0) return 1 / solve(x, -n);
-9        if (n % 2 == 0) return solve(x * x, n / 2);
-10        else return x * solve(x * x, n / 2);
+2    public List<String> fizzBuzz(int n) {
+3        List<String> ans=new ArrayList<>();
+4        for(int i=1;i<=n;i++){
+5            if(i%3==0 && i%5==0) ans.add("FizzBuzz");
+6            else if(i%3==0 && i%5!=0) ans.add("Fizz");
+7            else if(i%3!=0 && i%5==0) ans.add("Buzz");
+8            else ans.add(Integer.toString(i));
+9        }
+10        return ans;
 11    }
 12}
-13
