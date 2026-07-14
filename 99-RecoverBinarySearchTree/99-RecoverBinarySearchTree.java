512bfef54daf7c03e498c46d3c3eea17f93d8434
ARUN T <arundevil2007@gmail.com>
2026-07-14 09:39:59 +0530

    File updated at 7/14/2026, 9:39:39 AM

--- 99-RecoverBinarySearchTree/99-RecoverBinarySearchTree.java
+++ 99-RecoverBinarySearchTree/99-RecoverBinarySearchTree.java
@@ -1,12 +1,35 @@
-// Last updated: 7/14/2026, 9:23:29 AM
-1class Solution {
-2    public double myPow(double x, int n) {
-3        return helper(x,(long) n);
-4    }
-5    public double helper(double x, long n){
-6        if(n==0) return 1;
-7        if(n<0) return 1/helper(x,-n);
-8        if(n%2==0) return helper(x*x,n/2);
-9        else return x*helper(x*x,n/2);
-10    }
-11}
+// Last updated: 7/14/2026, 9:39:39 AM
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    TreeNode f,s,p;
+18    public void recoverTree(TreeNode root) {
+19        inorder(root);
+20        int t=f.val;
+21        f.val=s.val;
+22        s.val=t;
+23    }
+24    public void inorder(TreeNode root){
+25        if(root==null) return;
+26        inorder(root.left);
+27        if(p!=null && p.val>root.val){
+28            if(f==null) f=p;
+29            s=root;
+30        }
+31        p=root;
+32        inorder(root.right);
+33    }
+34}
