d82060bde030eadcfac7af04e11a902326b7e847
ARUN T <arundevil2007@gmail.com>
2026-07-14 10:19:21 +0530

    File updated at 7/14/2026, 10:19:01 AM

--- 99-RecoverBinarySearchTree/99-RecoverBinarySearchTree.java
+++ 99-RecoverBinarySearchTree/99-RecoverBinarySearchTree.java
@@ -1,14 +1,18 @@
-// Last updated: 7/14/2026, 10:06:12 AM
-1class Solution {
-2    public void moveZeroes(int[] nums) {
-3        int n=nums.length,j=0;
-4        for(int i=0;i<n;i++){
-5            if(nums[i]!=0){
-6                int t=nums[j];
-7                nums[j]=nums[i];
-8                nums[i]=t;
-9                j++;
-10            }
-11        }
-12    }
-13}
+// Last updated: 7/14/2026, 10:19:01 AM
+class Solution {
+    public void moveZeroes(int[] nums) {
+
+        int k = 0;
+
+        for(int num : nums){
+
+            if(num != 0){
+                nums[k++] = num;
+            }
+        }
+
+        while(k < nums.length){
+            nums[k++] = 0;
+        }
+    }
+}
