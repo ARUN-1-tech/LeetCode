269436f346fb6953b234a520f9d805112fdcb787
ARUN T <arundevil2007@gmail.com>
2026-07-14 09:14:34 +0530

    File updated at 7/14/2026, 9:14:14 AM

--- 99-RecoverBinarySearchTree/99-RecoverBinarySearchTree.java
+++ 99-RecoverBinarySearchTree/99-RecoverBinarySearchTree.java
@@ -1,4 +1,4 @@
-// Last updated: 7/14/2026, 9:13:51 AM
+// Last updated: 7/14/2026, 9:14:14 AM
 1class Solution {
 2    public List<String> fizzBuzz(int n) {
 3        List<String> ans=new ArrayList<>();
@@ -6,7 +6,7 @@
 5            if(i%15==0) ans.add("FizzBuzz");
 6            else if(i%3==0) ans.add("Fizz");
 7            else if(i%5==0) ans.add("Buzz");
-8            else ans.add(Integer.toString(i));
+8            else ans.add(String.valueOf(i));
 9        }
 10        return ans;
 11    }
