4962d905cac0b96f467be074ac8fce8b2f04069e
ARUN T <arundevil2007@gmail.com>
2026-07-14 11:10:24 +0530

    File updated at 7/14/2026, 11:10:05 AM

--- 99-RecoverBinarySearchTree/99-RecoverBinarySearchTree.java
+++ 99-RecoverBinarySearchTree/99-RecoverBinarySearchTree.java
@@ -1,30 +1,14 @@
-// Last updated: 7/14/2026, 10:42:44 AM
-1/**
-2 * Definition for a binary tree node.
-3 * public class TreeNode {
-4 *     int val;
-5 *     TreeNode left;
-6 *     TreeNode right;
-7 *     TreeNode() {}
-8 *     TreeNode(int val) { this.val = val; }
-9 *     TreeNode(int val, TreeNode left, TreeNode right) {
-10 *         this.val = val;
-11 *         this.left = left;
-12 *         this.right = right;
-13 *     }
-14 * }
-15 */
-16class Solution {
-17    int s=0;
-18    public int findTilt(TreeNode root) {
-19        dfs(root);
-20        return s;
-21    }
-22    public int dfs(TreeNode root){
-23        if(root==null) return 0;
-24        int l=dfs(root.left);
-25        int r=dfs(root.right);
-26        s+=Math.abs(l-r);
-27        return l+r+root.val;
-28    }
-29}
+// Last updated: 7/14/2026, 11:10:05 AM
+1class Solution {
+2    public int subarraySum(int[] nums, int k) {
+3        int n=nums.length,a=0;
+4        for(int i=0;i<n;i++){
+5            int sum=0;
+6            for(int j=i;j<n;j++){
+7                sum+=nums[j];
+8                if(sum==k) a++;
+9            }
+10        }
+11        return a;
+12    }
+13}
