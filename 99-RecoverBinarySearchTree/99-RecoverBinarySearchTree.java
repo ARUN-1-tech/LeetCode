5f1b7974aeb902c61f7c5a8f1d4d9a6a8df6b324
ARUN T <arundevil2007@gmail.com>
2026-07-14 12:13:43 +0530

    File updated at 7/14/2026, 12:13:23 PM

--- 99-RecoverBinarySearchTree/99-RecoverBinarySearchTree.java
+++ 99-RecoverBinarySearchTree/99-RecoverBinarySearchTree.java
@@ -1,16 +1,26 @@
-// Last updated: 7/14/2026, 11:20:03 AM
+// Last updated: 7/14/2026, 12:13:23 PM
 1class Solution {
-2    public int maxArea(int[] height) {
-3        int n=height.length;
-4        int max=0;
-5        int i=0,j=n-1;
-6        while(i<j){
-7            int min=Math.min(height[i],height[j]);
-8            int s=(j-i)*min;
-9            max=Math.max(max,s);
-10            if(height[i]>height[j]) j--;
-11            else i++;
-12        }
-13        return max;
-14    }
-15}
+2    public List<List<Integer>> threeSum(int[] nums) {
+3        List<List<Integer>> result = new ArrayList<>();
+4        int n = nums.length;
+5        Arrays.sort(nums);
+6        for (int i = 0; i < n - 2; i++) {
+7            if (i > 0 && nums[i] == nums[i - 1]) continue;
+8            int left = i + 1;
+9            int right = n - 1;
+10            while (left < right) {
+11                int sum = nums[i] + nums[left] + nums[right];
+12                if (sum == 0) {
+13                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+14                    left++;
+15                    right--;
+16                    while (left < right && nums[left] == nums[left - 1]) left++;
+17                    while (left < right && nums[right] == nums[right + 1]) right--;
+18                }
+19                else if (sum < 0) left++;
+20                else right--;
+21            }
+22        }
+23        return result;
+24    }
+25}
