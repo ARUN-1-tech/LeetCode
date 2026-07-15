1248e246f6462e2e51b9bd440d96319f0034e1fd
ARUN T <arundevil2007@gmail.com>
2026-07-15 11:10:10 +0530

    File updated at 7/15/2026, 11:09:51 AM

--- 3000-MaximumAreaofLongestDiagonalRectangle/3000-MaximumAreaofLongestDiagonalRectangle.java
+++ 3000-MaximumAreaofLongestDiagonalRectangle/3000-MaximumAreaofLongestDiagonalRectangle.java
@@ -1,17 +1,17 @@
-// Last updated: 7/15/2026, 10:13:11 AM
+// Last updated: 7/15/2026, 11:09:51 AM
 1class Solution {
-2    public int[] decompressRLElist(int[] nums) {
-3        int k=0,size=0;
-4        for (int i = 0; i < nums.length; i += 2) {
-5            size += nums[i];
-6        }
-7        int[] ans = new int[size];
-8        int n=nums.length;
-9        for(int i=0;i<n;i+=2){
-10            while(nums[i]>0){
-11                ans[k++]=nums[i+1];
-12                nums[i]--;
-13            }
+2    public List<Integer> majorityElement(int[] nums) {
+3        Map<Integer,Integer> map=new HashMap<>();
+4        List<Integer> ans=new ArrayList<>();
+5        for(int num:nums){
+6            if(!map.containsKey(num)){
+7                map.put(num,1);
+8            }else{
+9                map.put(num,map.get(num)+1);
+10            }
+11        }
+12        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+13            if(entry.getValue() > nums.length/3) ans.add(entry.getKey());
 14        }
 15        return ans;
 16    }
