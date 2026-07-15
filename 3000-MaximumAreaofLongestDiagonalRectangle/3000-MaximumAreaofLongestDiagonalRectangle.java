d858854be0856fafa5e9710bff193740e1784a93
ARUN T <arundevil2007@gmail.com>
2026-07-15 10:08:05 +0530

    File updated at 7/15/2026, 10:07:46 AM

--- 3000-MaximumAreaofLongestDiagonalRectangle/3000-MaximumAreaofLongestDiagonalRectangle.java
+++ 3000-MaximumAreaofLongestDiagonalRectangle/3000-MaximumAreaofLongestDiagonalRectangle.java
@@ -1,16 +1,18 @@
-// Last updated: 7/15/2026, 9:37:57 AM
+// Last updated: 7/15/2026, 10:07:46 AM
 1class Solution {
-2    public int[][] modifiedMatrix(int[][] matrix){
-3        for(int i=0;i<matrix[0].length;i++){
-4            int x=0;
-5            for(int j=0;j<matrix.length;j++){
-6                x=Math.max(x,matrix[j][i]);
-7                
-8            }
-9            for(int a=0;a<matrix.length;a++){
-10                if(matrix[a][i]==-1) matrix[a][i]=x;
-11            } 
-12        }
-13        return matrix;
-14    }
-15}
+2    public int[] decompressRLElist(int[] nums) {
+3        int k=0,size=0;
+4        for (int i = 0; i < nums.length; i += 2) {
+5            size += nums[i];
+6        }
+7        int[] ans = new int[size];
+8        int n=nums.length;
+9        for(int i=0;i<n;i+=2){
+10            while(nums[i]>0){
+11                ans[k++]=nums[i+1];
+12                nums[i]--;
+13            }
+14        }
+15        return ans;
+16    }
+17}
