cd365c4388b1cc021b0acaef94473cd023313b2d
ARUN T <arundevil2007@gmail.com>
2026-07-15 12:24:42 +0530

    File updated at 7/15/2026, 12:24:23 PM

--- 3000-MaximumAreaofLongestDiagonalRectangle/3000-MaximumAreaofLongestDiagonalRectangle.java
+++ 3000-MaximumAreaofLongestDiagonalRectangle/3000-MaximumAreaofLongestDiagonalRectangle.java
@@ -1,15 +1,28 @@
-// Last updated: 7/15/2026, 11:23:50 AM
+// Last updated: 7/15/2026, 12:24:23 PM
 1class Solution {
-2    public void rotate(int[][] matrix) {
-3        int n=matrix.length,k=n-1;
-4        for(int i=0;i<n;i++){
-5            for(int j=i;j<k-i;j++){
-6                int temp=matrix[i][j];
-7                matrix[i][j]=matrix[k-j][i];
-8                matrix[k-j][i]=matrix[k-i][k-j];
-9                matrix[k-i][k-j]=matrix[j][k-i];
-10                matrix[j][k-i]=temp;
-11            }
-12        }
-13    }
-14}
+2    public List<List<Integer>> findWinners(int[][] mat) {
+3        Map<Integer,Integer> map=new HashMap<>();
+4        List<List<Integer>> ans=new ArrayList<>();
+5        for (int[] match: mat) {
+6            map.putIfAbsent(match[0], 0);
+7            map.put(match[1], map.getOrDefault(match[1], 0) + 1);
+8        }
+9        List<Integer> zeroLoss = new ArrayList<>();
+10        List<Integer> oneLoss = new ArrayList<>();
+11        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+12            int player = entry.getKey();
+13            int losses = entry.getValue();
+14            
+15            if (losses == 0) {
+16                zeroLoss.add(player);
+17            } else if (losses == 1) {
+18                oneLoss.add(player);
+19            }
+20        }
+21        Collections.sort(zeroLoss);
+22        Collections.sort(oneLoss);
+23        ans.add(zeroLoss);
+24        ans.add(oneLoss);
+25        return ans;
+26    }
+27}
