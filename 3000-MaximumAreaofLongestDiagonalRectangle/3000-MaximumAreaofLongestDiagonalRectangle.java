44945c7a2f2a96b1871fe0856b6efb1349f0a18c
ARUN T <arundevil2007@gmail.com>
2026-07-15 12:39:01 +0530

    File updated at 7/15/2026, 12:38:42 PM

--- 3000-MaximumAreaofLongestDiagonalRectangle/3000-MaximumAreaofLongestDiagonalRectangle.java
+++ 3000-MaximumAreaofLongestDiagonalRectangle/3000-MaximumAreaofLongestDiagonalRectangle.java
@@ -1,28 +1,22 @@
-// Last updated: 7/15/2026, 12:24:23 PM
+// Last updated: 7/15/2026, 12:38:42 PM
 1class Solution {
-2    public List<List<Integer>> findWinners(int[][] mat) {
-3        Map<Integer,Integer> map=new HashMap<>();
-4        List<List<Integer>> ans=new ArrayList<>();
-5        for (int[] match: mat) {
-6            map.putIfAbsent(match[0], 0);
-7            map.put(match[1], map.getOrDefault(match[1], 0) + 1);
-8        }
-9        List<Integer> zeroLoss = new ArrayList<>();
-10        List<Integer> oneLoss = new ArrayList<>();
-11        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-12            int player = entry.getKey();
-13            int losses = entry.getValue();
-14            
-15            if (losses == 0) {
-16                zeroLoss.add(player);
-17            } else if (losses == 1) {
-18                oneLoss.add(player);
-19            }
-20        }
-21        Collections.sort(zeroLoss);
-22        Collections.sort(oneLoss);
-23        ans.add(zeroLoss);
-24        ans.add(oneLoss);
-25        return ans;
-26    }
-27}
+2    public long largestPerimeter(int[] nums) {
+3        long ans=0,s=0;
+4        for(int num:nums){
+5            s+=num;
+6        }
+7        Arrays.sort(nums);
+8        int j=nums.length-1;
+9        while (j >= 2 && (s - nums[j]) <= nums[j]) {
+10            s=s-nums[j];
+11            j=j-1;
+12        }
+13        for(int i=0;i<=j;i++){
+14            ans+=nums[i];
+15        }
+16        if (j < 2) {
+17            return -1;
+18        }
+19        return ans;
+20    }
+21}
