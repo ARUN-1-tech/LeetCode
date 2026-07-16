2a07be39f743363f7a6bdc04ec7a4579e5b3f243
ARUN T <arundevil2007@gmail.com>
2026-07-16 09:39:49 +0530

    Create 229-MajorityElementII

---
+++ 229-MajorityElementII/229-MajorityElementII.java
@@ -0,0 +1,18 @@
+// Last updated: 7/16/2026, 9:39:30 AM
+1class Solution {
+2    public List<Integer> majorityElement(int[] nums) {
+3        Map<Integer,Integer> map=new HashMap<>();
+4        List<Integer> ans=new ArrayList<>();
+5        for(int num:nums){
+6            if(!map.containsKey(num)){
+7                map.put(num,1);
+8            }else{
+9                map.put(num,map.get(num)+1);
+10            }
+11        }
+12        for(Map.Entry<Integer,Integer> entry:map.entrySet()){
+13            if(entry.getValue()>nums.length/3) ans.add(entry.getKey());
+14        }
+15        return ans;
+16    }
+17}
