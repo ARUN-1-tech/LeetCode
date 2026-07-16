a65ef72f432777b8591d333f3d4e1e3aa907c82d
ARUN T <arundevil2007@gmail.com>
2026-07-16 11:43:12 +0530

    File updated at 7/16/2026, 11:42:53 AM

--- 229-MajorityElementII/229-MajorityElementII.java
+++ 229-MajorityElementII/229-MajorityElementII.java
@@ -1,20 +1,20 @@
-// Last updated: 7/16/2026, 10:46:31 AM
+// Last updated: 7/16/2026, 11:42:53 AM
 1class Solution {
-2    public long maximumSubarraySum(int[] nums, int k) {
-3        Map<Integer,Long> map=new HashMap<>();
-4        long c=0,m=Long.MIN_VALUE,cou=0;
-5        for(int num:nums){
-6            c+=num;
-7            if(map.containsKey(num-k)){
-8                m=Math.max(m,c-map.get(num-k));
-9            }
-10            if(map.containsKey(num+k)){
-11                m=Math.max(m,c-map.get(num+k));
-12            }
-13            if(!map.containsKey(num) || (c - num) < map.get(num)){
-14                map.put(num,c-num);
-15            }
-16        }
-17        return m==Long.MIN_VALUE?0:m;
+2    public int maximumSetSize(int[] nums1, int[] nums2) {
+3        Set<Integer> s1 = new HashSet(); 
+4        Set<Integer> s2 = new HashSet();
+5        Set<Integer> s3 = new HashSet(); 
+6        for (int num: nums1){
+7            s1.add(num);
+8            s3.add(num);
+9        } 
+10        for (int num: nums2) {
+11            s2.add(num);
+12            s3.add(num);
+13        } 
+14        int a=Math.min(s1.size(), nums1.length/2);
+15        int b=Math.min(s2.size(), nums2.length/2);
+16        int c=s3.size();
+17        return Math.min(a + b, c);                            
 18    }
 19}
