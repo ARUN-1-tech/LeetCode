8083617f1b39d383414203e56017b29443aa3b91
ARUN T <arundevil2007@gmail.com>
2026-07-16 10:46:50 +0530

    File updated at 7/16/2026, 10:46:31 AM

--- 229-MajorityElementII/229-MajorityElementII.java
+++ 229-MajorityElementII/229-MajorityElementII.java
@@ -1,18 +1,20 @@
-// Last updated: 7/16/2026, 9:39:30 AM
+// Last updated: 7/16/2026, 10:46:31 AM
 1class Solution {
-2    public List<Integer> majorityElement(int[] nums) {
-3        Map<Integer,Integer> map=new HashMap<>();
-4        List<Integer> ans=new ArrayList<>();
+2    public long maximumSubarraySum(int[] nums, int k) {
+3        Map<Integer,Long> map=new HashMap<>();
+4        long c=0,m=Long.MIN_VALUE,cou=0;
 5        for(int num:nums){
-6            if(!map.containsKey(num)){
-7                map.put(num,1);
-8            }else{
-9                map.put(num,map.get(num)+1);
-10            }
-11        }
-12        for(Map.Entry<Integer,Integer> entry:map.entrySet()){
-13            if(entry.getValue()>nums.length/3) ans.add(entry.getKey());
-14        }
-15        return ans;
-16    }
-17}
+6            c+=num;
+7            if(map.containsKey(num-k)){
+8                m=Math.max(m,c-map.get(num-k));
+9            }
+10            if(map.containsKey(num+k)){
+11                m=Math.max(m,c-map.get(num+k));
+12            }
+13            if(!map.containsKey(num) || (c - num) < map.get(num)){
+14                map.put(num,c-num);
+15            }
+16        }
+17        return m==Long.MIN_VALUE?0:m;
+18    }
+19}
