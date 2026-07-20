37cebe083a4b1a35348d4efb36fc5e023495e81e
ARUN T <arundevil2007@gmail.com>
2026-07-20 15:09:58 +0530

    File updated at 7/20/2026, 3:09:40 PM

--- 189-RotateArray/189-RotateArray.java
+++ 189-RotateArray/189-RotateArray.java
@@ -1,22 +1,18 @@
-// Last updated: 7/20/2026, 2:35:19 PM
+// Last updated: 7/20/2026, 3:09:40 PM
 1class Solution {
-2    public long largestPerimeter(int[] nums) {
-3        long ans=0,s=0;
-4        for(int num:nums){
-5            s+=num;
+2    public boolean uniqueOccurrences(int[] arr) {
+3        Map<Integer,Integer> map=new HashMap<>();
+4        for(int n:arr){
+5                map.put(n,map.getOrDefault(n,0)+1);
 6        }
-7        Arrays.sort(nums);
-8        int j=nums.length-1;
-9        while (j >= 2 && (s - nums[j]) <= nums[j]) {
-10            s=s-nums[j];
-11            j=j-1;
-12        }
-13        for(int i=0;i<=j;i++){
-14            ans+=nums[i];
-15        }
-16        if (j < 2) {
-17            return -1;
-18        }
-19        return ans;
-20    }
-21}
+7        Set<Integer> set=new HashSet<>();
+8        for(int val:map.values()){
+9            if(set.contains(val)){
+10                return false;
+11            }
+12            set.add(val);
+13        }
+14        return true;
+15
+16    }
+17}
