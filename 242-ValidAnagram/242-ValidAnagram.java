2254f79265925da8f6346d4c49329e57c2a6372b
ARUN T <arundevil2007@gmail.com>
2026-07-21 10:07:12 +0530

    File updated at 7/21/2026, 10:06:55 AM

--- 242-ValidAnagram/242-ValidAnagram.java
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -1,35 +1,12 @@
-// Last updated: 7/9/2026, 10:05:23 AM
-class Solution {
-    public boolean isAnagram(String s, String t) {
-        if(s.length() !=t.length()) return false;
-        // HashMap<Character,Integer> map=new HashMap<>();
-        // for(int i=0;i<s.length();i++){
-        //     char a=s.charAt(i);
-        //     char b=t.charAt(i);
-        //     if(!map.containsKey(a)){
-        //         map.put(a,0);
-        //     }if(!map.containsKey(b)){
-        //         map.put(b,0);
-        //     }if(map.containsKey(a)){
-        //         map.put(a,map.get(a)+1);
-        //     }if(map.containsKey(b)){
-        //         map.put(b,map.get(b)-1);
-        //     }
-        // }
-        // for(int i=0;i<s.length();i++){
-        //     char c=s.charAt(i);
-        //     if(map.get(c)!=0) return false;
-        // }
-        // return true;
-
-        int[] c=new int[26];
-        for(int i=0;i<s.length();i++){
-            c[s.charAt(i)-'a']++;
-            c[t.charAt(i)-'a']--;
-        }
-        for(int n:c){
-            if(n!=0) return false;
-        }
-        return true;
-    }
-}
+// Last updated: 7/21/2026, 10:06:55 AM
+1class Solution {
+2    public boolean isAnagram(String s, String t) {
+3        char[] arrs = s.toCharArray();
+4        char[] arrt = t.toCharArray();
+5        Arrays.sort(arrs);
+6        Arrays.sort(arrt);
+7        if (Arrays.equals(arrs, arrt))
+8            return true;
+9        return false;
+10    }
+11}
