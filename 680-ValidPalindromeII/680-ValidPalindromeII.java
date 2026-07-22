98138de945fc8290b3e31488d88b4c9d67be7b66
ARUN T <arundevil2007@gmail.com>
2026-07-22 09:01:26 +0530

    File updated at 7/22/2026, 9:01:09 AM

--- 680-ValidPalindromeII/680-ValidPalindromeII.java
+++ 680-ValidPalindromeII/680-ValidPalindromeII.java
@@ -1,29 +1,27 @@
-// Last updated: 7/22/2026, 8:59:15 AM
+// Last updated: 7/22/2026, 9:01:09 AM
 1class Solution {
 2    public boolean removeOneChar(String s,int left, int right){
 3        while(left<=right){
-4            if(s.charAt(left)!=s.charAt(right)){
-5                return false;
-6            }
-7            else{
-8                left++;
-9                right--;
-10            }
-11        }
-12        return true;
-13    }
-14    public boolean validPalindrome(String s) {
-15        int left=0;
-16        int right=s.length()-1;
-17        while(left<=right){
-18            if(s.charAt(left)!=s.charAt(right)){
-19                return removeOneChar(s,left+1,right) || removeOneChar(s,left,right-1)  ;
-20            }
-21            else{
-22                left++;
-23                right--;
-24            }
-25        }
-26        return true;
-27    }
-28}
+4            if(s.charAt(left)!=s.charAt(right)) return false;
+5            else{
+6                left++;
+7                right--;
+8            }
+9        }
+10        return true;
+11    }
+12    public boolean validPalindrome(String s) {
+13        int left=0;
+14        int right=s.length()-1;
+15        while(left<=right){
+16            if(s.charAt(left)!=s.charAt(right)){
+17                return removeOneChar(s,left+1,right) || removeOneChar(s,left,right-1)  ;
+18            }
+19            else{
+20                left++;
+21                right--;
+22            }
+23        }
+24        return true;
+25    }
+26}
