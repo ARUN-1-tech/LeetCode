0f6e2a442820fe20d814fa670205363841bf9add
ARUN T <arundevil2007@gmail.com>
2026-07-23 14:59:43 +0530

    File updated at 7/23/2026, 2:59:26 PM

--- 205-IsomorphicStrings/205-IsomorphicStrings.java
+++ 205-IsomorphicStrings/205-IsomorphicStrings.java
@@ -1,17 +1,19 @@
-// Last updated: 7/23/2026, 2:40:13 PM
+// Last updated: 7/23/2026, 2:59:26 PM
 1class Solution {
-2    public boolean isIsomorphic(String s, String t) {
-3        Map<Character,Character> map=new HashMap<>();
-4        for(int i=0;i<s.length();i++){
-5            char a=s.charAt(i);
-6            char b=t.charAt(i);
-7            if(!map.containsKey(a)){
-8                if(map.containsValue(b)) return false;
-9                map.put(a,b);
-10            }else{
-11                if(map.get(a)!=b) return false;
-12            }
-13        }
-14        return true;
-15    }
-16}
+2    public boolean wordPattern(String s, String t) {
+3        String[] str=t.split(" ");
+4        if(s.length()!=str.length) return false;
+5        Map<Character,String> map=new HashMap<>();
+6        for(int i=0;i<s.length();i++){
+7            char a=s.charAt(i);
+8            String b=str[i];
+9            if(!map.containsKey(a)){
+10                if(map.containsValue(b)) return false;
+11                map.put(a,b);
+12            }else{
+13                if(!map.get(a).equals(b)) return false;
+14            }
+15        }
+16        return true;
+17    }
+18}
