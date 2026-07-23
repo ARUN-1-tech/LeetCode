1f983021e32f371a62075af96dc27c833b4c836c
ARUN T <arundevil2007@gmail.com>
2026-07-23 15:12:19 +0530

    File updated at 7/23/2026, 3:12:01 PM

--- 205-IsomorphicStrings/205-IsomorphicStrings.java
+++ 205-IsomorphicStrings/205-IsomorphicStrings.java
@@ -1,19 +1,15 @@
-// Last updated: 7/23/2026, 2:59:26 PM
+// Last updated: 7/23/2026, 3:12:01 PM
 1class Solution {
-2    public boolean wordPattern(String s, String t) {
-3        String[] str=t.split(" ");
-4        if(s.length()!=str.length) return false;
-5        Map<Character,String> map=new HashMap<>();
-6        for(int i=0;i<s.length();i++){
-7            char a=s.charAt(i);
-8            String b=str[i];
-9            if(!map.containsKey(a)){
-10                if(map.containsValue(b)) return false;
-11                map.put(a,b);
-12            }else{
-13                if(!map.get(a).equals(b)) return false;
-14            }
-15        }
-16        return true;
-17    }
-18}
+2    public boolean canConstruct(String r, String m) {
+3        if(r.length()>m.length()) return false;
+4        int[] ans=new int[26];
+5        for(int i=0;i<m.length();i++){
+6            ans[m.charAt(i)-'a']++;
+7        }
+8        for(int i=0;i<r.length();i++){
+9            if(ans[r.charAt(i)-'a']==0) return false;
+10            ans[r.charAt(i)-'a']--;
+11        }
+12        return true;
+13    }
+14}
