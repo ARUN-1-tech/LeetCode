48f96ba9bfab62dd1441348fd8b70093368091af
ARUN T <arundevil2007@gmail.com>
2026-07-23 21:37:51 +0530

    File updated at 7/23/2026, 9:37:34 PM

--- 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.java
+++ 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.java
@@ -1,24 +1,30 @@
-// Last updated: 7/23/2026, 9:35:53 PM
+// Last updated: 7/23/2026, 9:37:34 PM
 1class Solution {
 2    public int search(int[] nums, int target) {
-3        int i=0,j=nums.length-1;
-4        while(i<=j){
-5            int mid=(i+j)/2;
-6            if(nums[mid]==target) return mid;
-7            if (nums[i] <= nums[mid]) {
-8                if (nums[i] <= target && target < nums[mid]) {
-9                    j = mid - 1;
-10                } else {
-11                    i = mid + 1;
-12                }
-13            } else {
-14                if (nums[mid] < target && target <= nums[j]) {
-15                    i = mid + 1;
-16                } else {
-17                    j = mid - 1;
-18                }
-19            }
-20        }
-21        return -1;
-22    }
-23}
+3        // int i = 0, j = nums.length - 1;
+4        // while (i <= j) {
+5        //     int mid = (i + j) / 2;
+6        //     if (nums[mid] == target)
+7        //         return mid;
+8        //     if (nums[i] <= nums[mid]) {
+9        //         if (nums[i] <= target && target < nums[mid]) {
+10        //             j = mid - 1;
+11        //         } else {
+12        //             i = mid + 1;
+13        //         }
+14        //     } else {
+15        //         if (nums[mid] < target && target <= nums[j]) {
+16        //             i = mid + 1;
+17        //         } else {
+18        //             j = mid - 1;
+19        //         }
+20        //     }
+21        // }
+22        // return -1;
+23
+24        for(int i=0;i<nums.length;i++){
+25            if(nums[i]==target) return i;
+26        }
+27        return -1;
+28    }
+29}
