e6a9937c9bf5484a1a08ce646e096be1f073850f
ARUN T <arundevil2007@gmail.com>
2026-07-23 14:40:30 +0530

    File updated at 7/23/2026, 2:40:13 PM

--- 205-IsomorphicStrings/205-IsomorphicStrings.java
+++ 205-IsomorphicStrings/205-IsomorphicStrings.java
@@ -1,23 +1,17 @@
-// Last updated: 7/23/2026, 2:19:22 PM
+// Last updated: 7/23/2026, 2:40:13 PM
 1class Solution {
 2    public boolean isIsomorphic(String s, String t) {
-3        HashMap<Character, Integer> charIndexS = new HashMap<>();
-4        HashMap<Character, Integer> charIndexT = new HashMap<>();
-5
-6        for (int i = 0; i < s.length(); i++) {
-7            if (!charIndexS.containsKey(s.charAt(i))) {
-8                charIndexS.put(s.charAt(i), i);
-9            }
-10
-11            if (!charIndexT.containsKey(t.charAt(i))) {
-12                charIndexT.put(t.charAt(i), i);
-13            }
-14
-15            if (!charIndexS.get(s.charAt(i)).equals(charIndexT.get(t.charAt(i)))) {
-16                return false;
-17            }
-18        }
-19
-20        return true;        
-21    }
-22}
+3        Map<Character,Character> map=new HashMap<>();
+4        for(int i=0;i<s.length();i++){
+5            char a=s.charAt(i);
+6            char b=t.charAt(i);
+7            if(!map.containsKey(a)){
+8                if(map.containsValue(b)) return false;
+9                map.put(a,b);
+10            }else{
+11                if(map.get(a)!=b) return false;
+12            }
+13        }
+14        return true;
+15    }
+16}
