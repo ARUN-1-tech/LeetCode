ee17eb5d9e262b6f5d61715724148a35568ebe72
ARUN T <arundevil2007@gmail.com>
2026-07-24 14:48:09 +0530

    File updated at 7/24/2026, 2:47:52 PM

--- 151-ReverseWordsinaString/151-ReverseWordsinaString.java
+++ 151-ReverseWordsinaString/151-ReverseWordsinaString.java
@@ -1,32 +1,29 @@
-// Last updated: 7/24/2026, 2:45:21 PM
+// Last updated: 7/24/2026, 2:47:52 PM
 1class Solution {
 2    public String reverseWords(String s) {
 3        List<String> lis = new ArrayList<>();
 4        int i = 0, j = 0;
-5        
-6        while (j < s.length()) {
-7            if (s.charAt(i) != ' ' && (j + 1 == s.length() || s.charAt(j + 1) == ' ')) {
-8                lis.add(s.substring(i, j + 1));
-9                i = j + 1;
-10                j++;
-11                continue;
-12            }
-13            
-14            if (s.charAt(i) == ' ') {
-15                i++;
-16                j = i;
-17            } else {
-18                j++;
-19            }
-20        }
-21        
-22        String res = "";
-23        for (int k = lis.size() - 1; k >= 0; k--) {
-24            res += lis.get(k);
-25            if (k > 0) {
-26                res += " ";
-27            }
-28        }
-29        return res;
-30    }
-31}
+5        while (j < s.length()) {
+6            if (s.charAt(i) != ' ' && (j + 1 == s.length() || s.charAt(j + 1) == ' ')) {
+7                lis.add(s.substring(i, j + 1));
+8                i = j + 1;
+9                j++;
+10                continue;
+11            }
+12            if (s.charAt(i) == ' ') {
+13                i++;
+14                j = i;
+15            } else {
+16                j++;
+17            }
+18        }
+19        StringBuilder res = new StringBuilder();
+20        for (int k = lis.size() - 1; k >= 0; k--) {
+21            res.append(lis.get(k));
+22            if (k > 0) {
+23                res.append(" ");
+24            }
+25        }
+26        return res.toString();
+27    }
+28}
