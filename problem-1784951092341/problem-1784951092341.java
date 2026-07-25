f85074cf30ef0d14327ac6446a780d0d01151f30
ARUN T <arundevil2007@gmail.com>
2026-07-25 09:15:09 +0530

    Create

---
+++ problem-1784951092341/problem-1784951092341.java
@@ -0,0 +1,17 @@
+// Last updated: 7/25/2026, 9:14:52 AM
+1class Solution {
+2    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+3        int n=nums1.length;
+4        int m=nums2.length;
+5        int[] ans=new int[n+m];
+6        int i=0,j=0,k=0;
+7        while(i<n && j<m){
+8            if(nums1[i]<nums2[j]) ans[k++]=nums1[i++];
+9            else ans[k++]=nums2[j++];
+10        }
+11        while(i<n) ans[k++]=nums1[i++];
+12        while(j<m) ans[k++]=nums2[j++];
+13        if((m+n)%2==1) return ans[(m+n)/2];
+14        return (ans[((m+n)/2)-1]+ans[(m+n)/2])/2.0;
+15    }
+16}
