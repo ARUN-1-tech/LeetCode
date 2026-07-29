ddc074db41e01ea0544f818a98a89a8af10501d7
ARUN T <arundevil2007@gmail.com>
2026-07-29 09:47:38 +0530

    File updated at 7/29/2026, 9:47:22 AM

--- 389-FindtheDifference/389-FindtheDifference.java
+++ 389-FindtheDifference/389-FindtheDifference.java
@@ -1,19 +1,21 @@
-// Last updated: 7/29/2026, 9:37:33 AM
+// Last updated: 7/29/2026, 9:47:22 AM
 1class Solution {
-2    public int[] intersection(int[] nums1, int[] nums2) {
-3        Set<Integer> map=new HashSet<>();
-4        List<Integer> ans=new ArrayList<>();
-5        for(int num:nums2) map.add(num);
-6        for(int num:nums1){
-7            if(map.contains(num)) {
-8                ans.add(num);
-9                map.remove(num);
-10            }
-11        }
-12        int[] result = new int[ans.size()];
-13        for (int i = 0; i < ans.size(); i++) {
-14            result[i] = ans.get(i);
-15        }
-16        return result;
-17    }
-18}
+2    public int[] intersect(int[] nums1, int[] nums2) {
+3        int i=0,j=0,k=0;
+4        Arrays.sort(nums1);
+5        Arrays.sort(nums2);
+6        while(i<nums1.length && j<nums2.length){
+7            if(nums1[i]>nums2[j]) j++;
+8            else if(nums1[i]<nums2[j]) i++;
+9            else{
+10                nums1[k++]=nums1[i++];
+11                j++;
+12            }
+13        }
+14        int[] result = new int[k];
+15        for (int z = 0; z < k; z++) {
+16            result[z] = nums1[z];
+17        }
+18        return result;
+19    }
+20}
