8f9a227beb509cc631a7d13ddbe018e6f74ddb66
ARUN T <arundevil2007@gmail.com>
2026-07-29 09:33:54 +0530

    File updated at 7/29/2026, 9:33:37 AM

--- 389-FindtheDifference/389-FindtheDifference.java
+++ 389-FindtheDifference/389-FindtheDifference.java
@@ -1,13 +1,22 @@
-// Last updated: 7/29/2026, 9:20:51 AM
+// Last updated: 7/29/2026, 9:33:37 AM
 1class Solution {
-2    public char findTheDifference(String s, String t) {
-3        int su=0,d=0;
-4        for(int i:t.toCharArray()){
-5            su+=i-'a';
-6        }
-7        for(int j:s.toCharArray()){
-8            d+=j-'a';
-9        }
-10        return (char)(su-d+'a');
-11    }
-12}
+2    public int[] intersection(int[] nums1, int[] nums2) {
+3        Set<Integer> map1=new HashSet<>();
+4        Set<Integer> map2=new HashSet<>();
+5        List<Integer> ans=new ArrayList<>();
+6        for(int num:nums1){
+7            if(!map1.contains(num)) map1.add(num);
+8        }
+9        for(int n:nums2){
+10            if(!map2.contains(n)) map2.add(n);
+11        }
+12        for(int num:map1){
+13            if(map2.contains(num)) ans.add(num);
+14        }
+15        int[] result = new int[ans.size()];
+16        for (int i = 0; i < ans.size(); i++) {
+17            result[i] = ans.get(i);
+18        }
+19        return result;
+20    }
+21}
