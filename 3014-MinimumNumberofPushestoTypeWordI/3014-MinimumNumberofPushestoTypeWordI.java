158db82a161a20fa885752560fb0df8f6e939e2b
ARUN T <arundevil2007@gmail.com>
2026-07-30 09:20:22 +0530

    File updated at 7/30/2026, 9:20:06 AM

--- 3014-MinimumNumberofPushestoTypeWordI/3014-MinimumNumberofPushestoTypeWordI.java
+++ 3014-MinimumNumberofPushestoTypeWordI/3014-MinimumNumberofPushestoTypeWordI.java
@@ -1,15 +1,17 @@
-// Last updated: 7/30/2026, 9:18:31 AM
+// Last updated: 7/30/2026, 9:20:06 AM
 1class Solution {
-2    public int maxProduct(int[] nums) {
-3        int a=0,b=0;
-4        for(int i=0;i<nums.length;i++){
-5            if(nums[i]>a){
-6                b=a;
-7                a=nums[i];
-8            }else if(nums[i]>b){
-9                b=nums[i];
-10            }
-11        }
-12        return (a-1)*(b-1);
-13    }
-14}
+2    public int maxProduct(int n) {
+3        int s=0,a=0,b=0;
+4        while(n>0){
+5            s=n%10;
+6            if(s>a){
+7                b=a;
+8                a=s;
+9            }else if(s>b){
+10                b=s;
+11            }
+12            n=n/10;
+13        }
+14        return a*b;
+15    }
+16}
