5940af02285de78737550608b93b8f35f6826ecb
ARUN T <arundevil2007@gmail.com>
2026-07-31 10:35:59 +0530

    File updated at 7/31/2026, 10:35:43 AM

--- 206-ReverseLinkedList/206-ReverseLinkedList.java
+++ 206-ReverseLinkedList/206-ReverseLinkedList.java
@@ -1,35 +1,27 @@
-// Last updated: 7/31/2026, 10:02:13 AM
+// Last updated: 7/31/2026, 10:35:43 AM
 1/**
 2 * Definition for singly-linked list.
-3 * public class ListNode {
+3 * class ListNode {
 4 *     int val;
 5 *     ListNode next;
-6 *     ListNode() {}
-7 *     ListNode(int val) { this.val = val; }
-8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-9 * }
-10 */
-11class Solution {
-12    public ListNode mergeTwoLists(ListNode head1, ListNode head2) {
-13        ListNode t1=head1;
-14        ListNode t2=head2;
-15        ListNode dummy = new ListNode(-1);
-16        ListNode ans = dummy;
-17        while(t1!=null && t2!=null){
-18            if(t1.val<=t2.val){
-19                ans.next=t1;
-20                t1=t1.next;
-21            }else{
-22                ans.next=t2;
-23                t2=t2.next;
-24            } 
-25            ans=ans.next;
-26        }
-27        if (t1 != null) {
-28            ans.next = t1;
-29        } else {
-30            ans.next = t2;
-31        }
-32        return dummy.next;
-33    }
-34}
+6 *     ListNode(int x) {
+7 *         val = x;
+8 *         next = null;
+9 *     }
+10 * }
+11 */
+12public class Solution {
+13    public boolean hasCycle(ListNode head) {
+14        if (head == null || head.next == null) {
+15            return false;
+16        }
+17        ListNode slow=head;
+18        ListNode fast=head;
+19        while(fast!=null && fast.next!=null){
+20            slow=slow.next;
+21            fast=fast.next.next;
+22            if(slow==fast) return true;
+23        }
+24        return false;
+25    }
+26}
