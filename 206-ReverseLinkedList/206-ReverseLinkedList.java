26b5094feaab05128e4521119ec7db6ee0c20d3e
ARUN T <arundevil2007@gmail.com>
2026-07-31 10:02:29 +0530

    File updated at 7/31/2026, 10:02:13 AM

--- 206-ReverseLinkedList/206-ReverseLinkedList.java
+++ 206-ReverseLinkedList/206-ReverseLinkedList.java
@@ -1,4 +1,4 @@
-// Last updated: 7/31/2026, 9:10:15 AM
+// Last updated: 7/31/2026, 10:02:13 AM
 1/**
 2 * Definition for singly-linked list.
 3 * public class ListNode {
@@ -10,15 +10,26 @@
 9 * }
 10 */
 11class Solution {
-12    public ListNode reverseList(ListNode head) {
-13        ListNode pre=null;
-14        ListNode cur=head;
-15        while(cur!=null){
-16        ListNode temp=cur.next;
-17            cur.next=pre;
-18            pre=cur;
-19            cur=temp;
-20        }
-21        return pre;
-22    }
-23}
+12    public ListNode mergeTwoLists(ListNode head1, ListNode head2) {
+13        ListNode t1=head1;
+14        ListNode t2=head2;
+15        ListNode dummy = new ListNode(-1);
+16        ListNode ans = dummy;
+17        while(t1!=null && t2!=null){
+18            if(t1.val<=t2.val){
+19                ans.next=t1;
+20                t1=t1.next;
+21            }else{
+22                ans.next=t2;
+23                t2=t2.next;
+24            } 
+25            ans=ans.next;
+26        }
+27        if (t1 != null) {
+28            ans.next = t1;
+29        } else {
+30            ans.next = t2;
+31        }
+32        return dummy.next;
+33    }
+34}
