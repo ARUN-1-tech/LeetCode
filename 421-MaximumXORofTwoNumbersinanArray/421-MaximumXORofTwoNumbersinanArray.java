b43012b63212cc4984ee41dbd9049b562a81b548
ARUN T <arundevil2007@gmail.com>
2026-08-03 14:49:17 +0530

    File updated at 8/3/2026, 2:49:01 PM

--- 421-MaximumXORofTwoNumbersinanArray/421-MaximumXORofTwoNumbersinanArray.java
+++ 421-MaximumXORofTwoNumbersinanArray/421-MaximumXORofTwoNumbersinanArray.java
@@ -1,17 +1,7 @@
-// Last updated: 8/3/2026, 2:41:16 PM
+// Last updated: 8/3/2026, 2:49:01 PM
 1class Solution {
-2    public String reverseVowels(String s) {
-3        String v="aeiouAEIOU";
-4        int i=0,j=s.length()-1;
-5        char[] ans=s.toCharArray();
-6        while(i<j){
-7            while(i<j && v.indexOf(ans[i])==-1) i++;
-8            while(i<j && v.indexOf(ans[j])==-1) j--;
-9            char c=ans[i];
-10            ans[i]=ans[j];
-11            ans[j]=c;
-12            i++;j--;
-13        }
-14        return new String(ans);
-15    }
-16}
+2    public boolean rotateString(String s, String goal) {
+3        if(s.length()!=goal.length()) return false;
+4        return (s+s).contains(goal);
+5    }
+6}
