2d0e6c4c2e9b1f5f6eb01f7787d4e5c59984d10c
ARUN T <arundevil2007@gmail.com>
2026-08-03 14:49:53 +0530

    File updated at 8/3/2026, 2:49:38 PM

--- 421-MaximumXORofTwoNumbersinanArray/421-MaximumXORofTwoNumbersinanArray.java
+++ 421-MaximumXORofTwoNumbersinanArray/421-MaximumXORofTwoNumbersinanArray.java
@@ -1,4 +1,4 @@
-// Last updated: 8/3/2026, 2:49:01 PM
+// Last updated: 8/3/2026, 2:49:38 PM
 1class Solution {
 2    public boolean rotateString(String s, String goal) {
 3        if(s.length()!=goal.length()) return false;
