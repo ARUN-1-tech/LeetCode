20d8498e65ba38338f3db37ac570a34bff703582
ARUN T <arundevil2007@gmail.com>
2026-08-06 14:48:51 +0530

    File updated at 8/6/2026, 2:48:37 PM

--- 237-DeleteNodeinaLinkedList/237-DeleteNodeinaLinkedList.java
+++ 237-DeleteNodeinaLinkedList/237-DeleteNodeinaLinkedList.java
@@ -1,41 +1,30 @@
-// Last updated: 8/6/2026, 2:38:07 PM
-1class MyQueue {
-2    Stack<Integer> s1=new Stack<>();
-3    Stack<Integer> s2=new Stack<>();
-4
-5    public MyQueue() {
-6    }
-7    
-8    public void push(int x) {
-9        while (!s2.isEmpty()) {
-10            s1.push(s2.pop());
-11        }
-12        
-13        s1.push(x);
-14        
-15        while (!s1.isEmpty()) {
-16            s2.push(s1.pop());
-17        }
-18    }
-19    
-20    public int pop() {
-21        return s2.pop();
-22    }
-23    
-24    public int peek() {
-25        return s2.peek();
-26    }
-27    
-28    public boolean empty() {
-29        return s2.isEmpty();
-30    }
-31}
-32
-33/**
-34 * Your MyQueue object will be instantiated and called as such:
-35 * MyQueue obj = new MyQueue();
-36 * obj.push(x);
-37 * int param_2 = obj.pop();
-38 * int param_3 = obj.peek();
-39 * boolean param_4 = obj.empty();
-40 */
+// Last updated: 8/6/2026, 2:48:37 PM
+1/*
+2// Definition for a Node.
+3class Node {
+4    public int val;
+5    public List<Node> children;
+6
+7    public Node() {}
+8
+9    public Node(int _val) {
+10        val = _val;
+11    }
+12
+13    public Node(int _val, List<Node> _children) {
+14        val = _val;
+15        children = _children;
+16    }
+17};
+18*/
+19
+20class Solution {
+21    List<Integer> ans = new ArrayList<>();
+22    public List<Integer> preorder(Node root) {
+23        if (root == null) return ans;
+24        ans.add(root.val);
+25        for (Node child : root.children)
+26            preorder(child);
+27        return ans;
+28    }
+29}
