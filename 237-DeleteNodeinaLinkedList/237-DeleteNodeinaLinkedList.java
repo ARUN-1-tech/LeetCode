8df42ecff4d84f37e48f867493efe4d927354ab7
ARUN T <arundevil2007@gmail.com>
2026-08-06 14:15:57 +0530

    File updated at 8/6/2026, 2:15:43 PM

--- 237-DeleteNodeinaLinkedList/237-DeleteNodeinaLinkedList.java
+++ 237-DeleteNodeinaLinkedList/237-DeleteNodeinaLinkedList.java
@@ -1,15 +1,22 @@
-// Last updated: 8/6/2026, 12:29:35 PM
-1/**
-2 * Definition for singly-linked list.
-3 * public class ListNode {
-4 *     int val;
-5 *     ListNode next;
-6 *     ListNode(int x) { val = x; }
-7 * }
-8 */
-9class Solution {
-10    public void deleteNode(ListNode node) {
-11        node.val=node.next.val;
-12        node.next=node.next.next;
-13    }
-14}
+// Last updated: 8/6/2026, 2:15:43 PM
+1class Solution {
+2    public String simplifyPath(String path) {
+3        String[] arr=path.split("/");
+4        Stack<String> st=new Stack<>();
+5        for(String ch:arr){
+6            if(ch.equals("") || ch.equals(".")) continue;
+7            else if(ch.equals("..")){
+8                if(!st.isEmpty()){
+9                    st.pop();
+10                }
+11            }
+12            else st.push(ch);
+13        }
+14        StringBuilder sc=new StringBuilder();
+15        for(int i=0;i<=st.size()-1;i++){
+16            sc.append("/").append(st.get(i));
+17        }
+18        if(st.size()<=0) return "/";
+19        return new String(sc);
+20    }
+21}
