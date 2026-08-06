7627014ffbdaa77c61b50170812e08340e3d1723
ARUN T <arundevil2007@gmail.com>
2026-08-06 14:38:21 +0530

    File updated at 8/6/2026, 2:38:07 PM

--- 237-DeleteNodeinaLinkedList/237-DeleteNodeinaLinkedList.java
+++ 237-DeleteNodeinaLinkedList/237-DeleteNodeinaLinkedList.java
@@ -1,35 +1,41 @@
-// Last updated: 8/6/2026, 2:33:51 PM
-1class MyStack {
-2    Queue<Integer> q;
-3    public MyStack() {
-4        q=new LinkedList<>();
-5    }
-6    
-7    public void push(int x) {
-8        q.add(x);
-9        for(int i=1;i<q.size();i++){
-10            q.add(q.remove());
+// Last updated: 8/6/2026, 2:38:07 PM
+1class MyQueue {
+2    Stack<Integer> s1=new Stack<>();
+3    Stack<Integer> s2=new Stack<>();
+4
+5    public MyQueue() {
+6    }
+7    
+8    public void push(int x) {
+9        while (!s2.isEmpty()) {
+10            s1.push(s2.pop());
 11        }
-12    }
-13    
-14    public int pop() {
-15        return q.remove();
-16    }
-17    
-18    public int top() {
-19        return q.peek();
-20    }
-21    
-22    public boolean empty() {
-23        return q.isEmpty();
-24    }
-25}
-26
-27/**
-28 * Your MyStack object will be instantiated and called as such:
-29 * MyStack obj = new MyStack();
-30 * obj.push(x);
-31 * int param_2 = obj.pop();
-32 * int param_3 = obj.top();
-33 * boolean param_4 = obj.empty();
-34 */
+12        
+13        s1.push(x);
+14        
+15        while (!s1.isEmpty()) {
+16            s2.push(s1.pop());
+17        }
+18    }
+19    
+20    public int pop() {
+21        return s2.pop();
+22    }
+23    
+24    public int peek() {
+25        return s2.peek();
+26    }
+27    
+28    public boolean empty() {
+29        return s2.isEmpty();
+30    }
+31}
+32
+33/**
+34 * Your MyQueue object will be instantiated and called as such:
+35 * MyQueue obj = new MyQueue();
+36 * obj.push(x);
+37 * int param_2 = obj.pop();
+38 * int param_3 = obj.peek();
+39 * boolean param_4 = obj.empty();
+40 */
