846aaa95b4e78ddffc413a0eed61d52f3b18ea74
ARUN T <arundevil2007@gmail.com>
2026-08-08 12:16:37 +0530

    File updated at 8/8/2026, 12:16:23 PM

--- 821-ShortestDistancetoaCharacter/821-ShortestDistancetoaCharacter.java
+++ 821-ShortestDistancetoaCharacter/821-ShortestDistancetoaCharacter.java
@@ -1,33 +1,40 @@
-// Last updated: 8/8/2026, 12:11:00 PM
-1/**
-2 * Definition for singly-linked list.
-3 * public class ListNode {
-4 *     int val;
-5 *     ListNode next;
-6 *     ListNode() {}
-7 *     ListNode(int val) { this.val = val; }
-8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-9 * }
-10 */
-11class Solution {
-12    public ListNode partition(ListNode head, int x) {
-13        ListNode before=new ListNode(0);
-14        ListNode after=new ListNode(0);
-15        ListNode b=before;
-16        ListNode a=after;
-17        ListNode cur=head;
-18        while(cur!=null){
-19            if(cur.val<x){
-20                b.next=cur;
-21                b=b.next;
-22            }else{
-23                a.next=cur;
-24                a=a.next;
-25            }
-26            cur=cur.next;
-27        }
-28        a.next=null;
-29        b.next=after.next;
-30        return before.next;
-31    }
-32}
+// Last updated: 8/8/2026, 12:16:23 PM
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode p = head;
+        Queue<Integer> q1 = new LinkedList<>();
+        Queue<Integer> q2 = new LinkedList<>();
+        //segregating the elements according to the given value;
+        while(p!=null){
+            if(p.val<x){
+                q1.add(p.val);
+            }
+            else{
+                q2.add(p.val);
+            }
+            p=p.next;
+        }
+        
+        //putting the elements back
+        p = head;
+        while(!q1.isEmpty()){
+            p.val = q1.poll();
+            p=p.next;
+        }
+        while(!q2.isEmpty()){
+            p.val = q2.poll();
+            p=p.next;
+        }
+        return head;
+    }
+}
