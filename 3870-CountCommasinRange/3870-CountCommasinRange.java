c9b9ff48dea94b26a810b8c889e01d26b113d66f
ARUN T <arundevil2007@gmail.com>
2026-09-08 10:23:17 +0530

    File updated at 9/8/2026, 10:23:17 AM

--- 3870-CountCommasinRange/3870-CountCommasinRange.java
+++ 3870-CountCommasinRange/3870-CountCommasinRange.java
@@ -1,15 +1,12 @@
-// Last updated: 9/8/2026, 10:01:44 AM
+// Last updated: 9/8/2026, 10:23:17 AM
 1class Solution {
-2    public boolean checkDivisibility(int n) {
-3        int y=n;
-4        int s=0,p=1;
-5        while(n>0){
-6            int c=n%10;
-7            s+=c;
-8            p*=c;
-9            n=n/10;
-10        }
-11        int a=s+p;
-12        return y%a==0;
-13    }
-14}
+2    public int getMinDistance(int[] nums, int target, int start) {
+3        int min = Integer.MAX_VALUE;
+4        for (int i = 0; i < nums.length; i++) {
+5            if (nums[i] == target) {
+6                min = Math.min(min, Math.abs(start - i));
+7            }
+8        }
+9        return min;
+10    }
+11}
