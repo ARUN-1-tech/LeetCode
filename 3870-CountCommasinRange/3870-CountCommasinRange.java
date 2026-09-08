f065d67981458192213586db51aaf2e48427e9c4
ARUN T <arundevil2007@gmail.com>
2026-09-08 09:54:26 +0530

    File updated at 9/8/2026, 9:54:26 AM

--- 3870-CountCommasinRange/3870-CountCommasinRange.java
+++ 3870-CountCommasinRange/3870-CountCommasinRange.java
@@ -1,8 +1,14 @@
-// Last updated: 9/8/2026, 9:43:29 AM
+// Last updated: 9/8/2026, 9:54:26 AM
 1class Solution {
-2    public int countCommas(int n) {
-3        int x=999;
-4        if(n>x) return n-x;
-5        return 0;
-6    }
-7}
+2    public int missingMultiple(int[] nums, int k) {
+3        Set<Integer> st=new HashSet<>();
+4        for(int i:nums){
+5            st.add(i);
+6        }
+7        int x=k;
+8        while(st.contains(x)){
+9            x=x+k;
+10        }
+11        return x;
+12    }
+13}
