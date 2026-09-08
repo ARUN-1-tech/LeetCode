9436fb3efa89961dfd6d904913ab0514faf561f2
ARUN T <arundevil2007@gmail.com>
2026-09-08 10:01:44 +0530

    File updated at 9/8/2026, 10:01:44 AM

--- 3870-CountCommasinRange/3870-CountCommasinRange.java
+++ 3870-CountCommasinRange/3870-CountCommasinRange.java
@@ -1,14 +1,15 @@
-// Last updated: 9/8/2026, 9:54:26 AM
+// Last updated: 9/8/2026, 10:01:44 AM
 1class Solution {
-2    public int missingMultiple(int[] nums, int k) {
-3        Set<Integer> st=new HashSet<>();
-4        for(int i:nums){
-5            st.add(i);
-6        }
-7        int x=k;
-8        while(st.contains(x)){
-9            x=x+k;
+2    public boolean checkDivisibility(int n) {
+3        int y=n;
+4        int s=0,p=1;
+5        while(n>0){
+6            int c=n%10;
+7            s+=c;
+8            p*=c;
+9            n=n/10;
 10        }
-11        return x;
-12    }
-13}
+11        int a=s+p;
+12        return y%a==0;
+13    }
+14}
